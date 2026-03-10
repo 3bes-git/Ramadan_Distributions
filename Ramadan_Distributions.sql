5eafcb905923849de19b62fffe5eb02833808159
3bes <eng.ahmed.fcai@gmail.com>
2026-03-10 21:59:31 +0200

    INSERT VALUES

--- Ramadan_Distributions.sql
+++ Ramadan_Distributions.sql
@@ -79,5 +79,58 @@ CREATE TABLE Driver_Training (
 );
 
 
-SELECT * FROM warehouses;
+INSERT INTO Users (full_name, gender, age, phone, address, role)
+VALUES
+('Ahmed Abbas','Male',20,'01012345678','Bilbeis','Admin'),
+('Ali Mahmoud','Male',35,'01055555555','Zagazig','Driver'),
+('Sara Adel','Female',28,'01099999999','Zagazig','Volunteer'),
+('Fatma Mohamed','Female',45,'01077777777','Minya Al-Qamh','Beneficiary');
+
+INSERT INTO Warehouses (name, location, max_capacity, supervisor_id)
+VALUES
+('Zagazig Warehouse','Zagazig',5000,1),
+('Bilbeis Warehouse','Bilbeis',8000,1);
+
+INSERT INTO Food_Categories (category_name, required_storage_temperature)
+VALUES
+('Dry',25.00),
+('Fresh',4.00),
+('Cooked',5.00);
+
+INSERT INTO Inventory_Items (item_name, quantity_kg, warehouse_id, category_id, expiry_date)
+VALUES
+('Rice',1000,1,1,'2026-04-10'),
+('Potatoes',500,1,2,'2026-03-15'),
+('Cooked Chicken',200,2,3,'2026-03-12');
+
+INSERT INTO Donations_Log (donor_name, amount_value, donation_type, org_type, donation_date)
+VALUES
+('Ahmed Ali',5000,'Cash','Individual','2026-03-01'),
+('Pepsi Company',20000,'Cash','Company','2026-03-05'),
+('Red Crescent',10000,'Cash','NGO','2026-03-07');
+
+
+INSERT INTO Beneficiary_Details (user_id, family_members_count, poverty_score, last_received_date)
+VALUES
+(4,5,9,'2026-02-20');
+
+
+INSERT INTO Training_Sessions (session_name, trainer_name, session_date)
+VALUES
+('Safety First','Mohamed Ali','2026-02-10'),
+('Emergency Driving','Ahmed Nabil','2026-02-15');
+
+
+INSERT INTO Driver_Training (driver_id, session_id)
+VALUES
+(2,1),
+(2,2);
+
 SELECT * FROM Users;
+SELECT * FROM warehouses;
+SELECT * FROM Inventory_Items;
+SELECT * FROM Food_Categories;
+SELECT * FROM Donations_Log;
+SELECT * FROM Beneficiary_Details;
+SELECT * FROM Training_Sessions;
+SELECT * FROM Driver_Training;
